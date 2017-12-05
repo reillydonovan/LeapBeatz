10e7a5d9d68c48b4548faa23ae0c6bc61b824221
reillydonovan <reillydonovan3@gmail.com>
2017-12-04 17:35:11 -0800

    Getting some OSC messages

---
+++ Assets/Scripts/CollisionTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTrigger : MonoBehaviour {
+
+    // Use this for initialization
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("On Trigger Enter");
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("On Trigger Stay");
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("On Trigger Exit");
+    }
+}
---
+++ Assets/Scripts/TransmitTransforms.cs
@@ -0,0 +1,43 @@
+﻿/* Copyright (c) 2017 ExT (V.Sigalkin) */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace extOSC.Examples
+{
+    public class TransmitTransforms : MonoBehaviour
+    {
+        #region Public Vars
+        public GameObject transformObject;
+        public string Address = "/example/1";
+
+        [Header("OSC Settings")]
+        public OSCTransmitter Transmitter;
+
+        #endregion
+
+        #region Unity Methods
+
+        protected virtual void Start()
+        { /*
+            var message = new OSCMessage(Address);
+            float transformObjectX = transformObject.transform.position.x;
+            message.AddValue(OSCValue.Float(transformObjectX));
+
+            Transmitter.Send(message);
+            */
+        }
+
+        protected virtual void Update()
+        {
+            var message = new OSCMessage(Address);
+            float transformObjectX = transformObject.transform.position.x * 10;
+            message.AddValue(OSCValue.Float(transformObjectX));
+
+            Transmitter.Send(message);
+        } 
+
+        #endregion
+    }
+}
