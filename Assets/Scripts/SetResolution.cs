2765c350510e5b45458f5b59652e5eb0fae806ff
reillydonovan <reillydonovan3@gmail.com>
2017-12-11 10:59:21 -0800

    added new ableton track

--- Assets/Scripts/SetResolution.cs
+++ Assets/Scripts/SetResolution.cs
@@ -6,7 +6,7 @@ public class SetResolution : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Screen.SetResolution(3840, 1080, false);
+        Screen.SetResolution(1920, 1080, false);
     }
 	
 	// Update is called once per frame
